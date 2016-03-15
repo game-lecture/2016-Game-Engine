6248467cdeed971be19f7e16e553fb71b0b1aa88
Dae-Hyun Lee <daehyun.lee@gmail.com>
2016-03-16 07:41:05 +0900

    Lab04 - MakerRenderWindow Project Upload

---
+++ Lab04/MakeRenderWindow/main.cpp
@@ -0,0 +1,66 @@
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+#define WIN32_LEAN_AND_MEAN
+#include "windows.h"
+#endif
+
+#include <Ogre.h>
+#include <OIS/OIS.h>
+
+using namespace Ogre;
+
+class ESCListener : public FrameListener {
+	OIS::Keyboard *mKeyboard;
+  
+public:
+	ESCListener(OIS::Keyboard *keyboard) : mKeyboard(keyboard) {}
+  bool frameStarted(const FrameEvent &evt)
+  {
+    mKeyboard->capture();
+	return !mKeyboard->isKeyDown(OIS::KC_ESCAPE);
+  }
+};  
+
+class LectureApp {
+
+  Root* mRoot;
+  RenderWindow* mWindow;
+  SceneManager* mSceneMgr;
+  Camera* mCamera;
+  Viewport* mViewport;
+  OIS::Keyboard* mKeyboard;
+  OIS::InputManager *mInputManager;
+  ESCListener* mESCListener;
+
+public:
+
+  LectureApp() {}
+
+  ~LectureApp() {}
+
+  void go(void)
+  {
+	// fill here
+  }
+};
+
+
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+INT WINAPI WinMain( HINSTANCE hInst, HINSTANCE, LPSTR strCmdLine, INT )
+#else
+int main(int argc, char *argv[])
+#endif
+{
+  LectureApp app;
+
+  try {
+    app.go();
+  } catch( Ogre::Exception& e ) {
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+    MessageBox( NULL, e.getFullDescription().c_str(), "An exception has occured!", MB_OK | MB_ICONERROR | MB_TASKMODAL);
+#else
+    std::cerr << "An exception has occured: " << e.getFullDescription().c_str() << std::endl;
+#endif
+  }
+
+  return 0;
+}
